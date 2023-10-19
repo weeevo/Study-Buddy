36c6e5e2291f820b59cf0870bd7156290adf6ad5
weeevo <will@wennik.com>
2023-10-19 11:37:50 -0400

    updates

--- scripts/content.js
+++ scripts/content.js
@@ -1,12 +1,12 @@
-// Create a div element for the blank screen
-const blankScreen = document.createElement('div');
-blankScreen.style.position = 'fixed';
-blankScreen.style.top = '0';
-blankScreen.style.left = '0';
-blankScreen.style.width = '100%';
-blankScreen.style.height = '100%';
-blankScreen.style.background = 'white'; // Set the background color you want
-blankScreen.style.zIndex = '9999'; // A high z-index to cover everything
+// // Create a div element for the blank screen
+// const blankScreen = document.createElement('div');
+// blankScreen.style.position = 'fixed';
+// blankScreen.style.top = '0';
+// blankScreen.style.left = '0';
+// blankScreen.style.width = '100%';
+// blankScreen.style.height = '100%';
+// blankScreen.style.background = 'white'; // Set the background color you want
+// blankScreen.style.zIndex = '9999'; // A high z-index to cover everything
 
-// Inject the div into the page
-document.body.appendChild(blankScreen);
+// // Inject the div into the page
+// document.body.appendChild(blankScreen);
